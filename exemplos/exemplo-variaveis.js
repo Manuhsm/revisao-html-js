0b444547c40671cb36fcc393f9abfad65d33a29b
MANUELA HELENA DOS SANTOS MELO <manuela_h_melo@estudante.sesisenai.org.br>
2026-02-11 22:01:29 -0300

    Revisão

---
+++ exemplos/exemplo-variaveis.js
@@ -0,0 +1,40 @@
+  function exemploVariaveis() {
+  var nome = "João Silva";
+  const idade = 18;
+
+  let possuiFaculdade = true;
+  possuiFaculdade = false;
+
+  var preco;
+  preco = 100;
+  // novo
+  let time; // Undefined
+  const comida = null; // Null
+  const novoObjeto = {}; // Object
+
+  console.log("O tipo de nome é: " + typeof nome);
+  console.log("O tipo de idade é: " + typeof idade);
+  console.log("O tipo de possuiFaculdade é: " + typeof possuiFaculdade);
+  console.log("O tipo de preco é: " + typeof preco);
+  console.log("O tipo de time é: " + typeof time);
+  console.log("O tipo de comida é: " + typeof comida);
+  console.log("O tipo de novoObjeto é: " + typeof novoObjeto);
+
+  const gols = 1000;
+  const frase1 = "Pelé fez " + gols + " gols";
+  const frase2 = `Pelé fez ${gols} gols`;
+
+  'JavaScript é "super" fácil';
+  "JavaScript é 'super' fácil";
+  'JavaScript é "super" fácil';
+  `JavaScript é "super" fácil"`;
+ // "JavaScript é "super" fácil"; // Inválido
+
+ const numeroUM = prompt ("Informa o número um:");
+ const numeroDois = prompt ("Informa o número dois:");
+
+ const soma = Number(numeroUM) + Number(numeroDois);
+    console.log("A soma é: " + soma);
+    
+
+}
