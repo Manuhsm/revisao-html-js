923113bedc8dfdfdbb23d5c16df0d4f5f13545bd
MANUELA HELENA DOS SANTOS MELO <manuela_h_melo@estudante.sesisenai.org.br>
2026-02-26 22:00:22 -0300

    Revisão html e js

---
+++ exemplos/data.js
@@ -0,0 +1,12 @@
+function exemploData(){
+    const agora = new Date();
+agora.getDate() // Dia
+agora.getMonth() // Número dia mês ** Começa em 0
+agora.getFullYear() // Ano
+agora.getHours() // Hora
+agora.getMinutes() // Minutos
+
+agora.getTime(); // getTime() mostra o tempo total em milissegundos desde: 1 de janeiro de 1970
+
+
+}
---
+++ exemplos/numeros-operadores.js
@@ -0,0 +1,27 @@
+function numeroOperadores(){
+    const idade = 50; 
+    const gols = 1000;
+    const pi = 3.14;
+
+const numero = 70;
+const unidade = 'kg';
+const peso = numero + unidade; // '80kg'
+const pesoPorDois = peso / 2; // NaN (Not a Number)
+
+const incremento = 5;
+console.log(incremento++); // 5
+console.log(incremento); // 6
+
+const incremento2 = 5;
+console.log(++incremento2); // 6
+console.log(incremento2); // 6
+
+if(possuiGraduacao) {
+  console.log('Possui graduação');
+} else {
+  console.log('Não possui graduação');
+}
+
+}
+
+
